e4506954c1c7bc8959591f30814a10e5987c340e
Victoria Umoh <victoriaumoh10@gmail.com>
2026-01-28 00:18:32 +0100

    fixes

--- src/app.js
+++ src/app.js
@@ -13,30 +13,30 @@ import adminRoutes from "./routes/admin.routes.js";
 const app = express();
 
 // CORS Configuration
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//   credentials: true
-// }));
-
-const allowedOrigins = [
-  'http://localhost:3000',               // Local React (Standard)
-  'http://localhost:5173',               // Local Vite (If you used Vite)
-  'https://fashion-frontend.netlify.app' // Live Production
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
+
+// const allowedOrigins = [
+//   'http://localhost:3000',               // Local React (Standard)
+//   'http://localhost:5173',               // Local Vite (If you used Vite)
+//   'https://fashion-frontend.netlify.app' // Live Production
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin) return callback(null, true);
+    
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
 
 app.use(express.json());
 app.use(morgan("dev"));
